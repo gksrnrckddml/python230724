d26cad989f0dc41f64b8c0b063b4102ff18377a9
gksrnrckddml <gksrnrckddml@naver.com>
2023-07-25 14:15:53 +0900

    클래스 연습1

--- ifelse.py
+++ ifelse.py
@@ -42,3 +42,19 @@ print("---range함수---")
 print(list(range(1,11)))
 print(list(range(2000,2024)))
 print(list(range(1,32)))
+
+for i in range(10):
+    print(i)
+
+print("---리스트컴프리헨션---")
+lst = list(range(1,11))
+print([i**2 for i in lst if i > 5])  #5보다 큰 수를 제곱하기(range는 끝 보다작은 수)
+tp = ("apple", "banana", "orange")  
+print ([len(i) for i in tp])   # 글자의 길이 추출
+d = {100:"apple", 200:"kiwi"}
+print([v.upper() for v in d.values()])    #
+
+for i in map(lambda x:x+10, lst):
+    print(i)
+
+
