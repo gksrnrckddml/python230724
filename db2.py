760fb3a078358fae765031e9b5c2d7e790c7f12a
gksrnrckddml <gksrnrckddml@naver.com>
2023-07-26 16:09:25 +0900

    수요일 오후 수업2

---
+++ db2.py
@@ -0,0 +1,36 @@
+# db2.py
+import sqlite3
+
+#연결객체 생성(일단은 메모리에서 연습)
+con = sqlite3.connect("c:\\work\\sample.db")
+#커서객체
+cur = con.cursor()
+#테이블 구조 생성
+cur.execute("create table if not exists PhoneBook" +
+    " (id integer primary key autoincrement, " +
+    " name text, phoneNum text);")
+#1건 입력
+cur.execute("insert into PhoneBook (name, phoneNum) values " +
+    " ('홍길동', '010-111');")
+#입력 파라메터 처리
+name = "전우치"
+phoneNumber = "010-222"
+cur.execute("insert into PhoneBook (name, phoneNum) values " +
+    " (?,?);", (name, phoneNumber))
+
+#다중의 행을 입력
+datalist = (("이순신", "010-333"), ("박문수", "010-123"))
+cur.executemany("insert into PhoneBook (name, phoneNum) values " +
+    " (?,?);", datalist)
+#검색구문
+cur.execute("select * from PhoneBook;")
+print(cur.fetchall())
+
+#작업완료
+con.commit()   #이게 있어야 작업을 하고 빠져나옴 아니면 fetchall해도 값이 추출되지 않음
+
+
+
+
+#for row in cur:
+#    print(row)
